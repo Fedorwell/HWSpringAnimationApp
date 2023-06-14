252bcf8468a77d367a4f889573cf31950fd030cb
Fedorwell <115953771+Fedorwell@users.noreply.github.com>
2023-06-14 21:31:17 +0300

    Add SingleTone

--- HWSpringAnimationApp/Services/DataStore.swift
+++ HWSpringAnimationApp/Services/DataStore.swift
@@ -6,3 +6,77 @@
 //
 
 import Foundation
+
+final class DataStore {
+
+let shared = DataStore()
+
+    let springAnimations = [
+           "pop",
+           "slideLeft",
+           "slideRight",
+           "slideDown",
+           "slideUp",
+           "squeezeLeft",
+           "squeezeRight",
+           "squeezeDown",
+           "squeezeUp",
+           "fadeIn",
+           "fadeOut",
+           "fadeOutIn",
+           "fadeInLeft",
+           "fadeInRight",
+           "fadeInDown",
+           "fadeInUp",
+           "zoomIn",
+           "zoomOut",
+           "fall",
+           "shake",
+           "flipX",
+           "flipY",
+           "morph",
+           "squeeze",
+           "flash",
+           "wobble",
+           "swing",
+            ]
+    
+    let springCurves = [
+        "easeIn",
+        "easeOut",
+        "easeInOut",
+        "linear",
+        "spring",
+        "easeInSine",
+        "easeOutSine",
+        "easeInOutSine",
+        "easeInQuad",
+        "easeOutQuad",
+        "easeInOutQuad",
+        "easeInCubic",
+        "easeOutCubic",
+        "easeInOutCubic",
+        "easeInQuart",
+        "easeOutQuart",
+        "easeInOutQuart",
+        "easeInQuint",
+        "easeOutQuint",
+        "easeInOutQuint",
+        "easeInExpo",
+        "easeOutExpo",
+        "easeInOutExpo",
+        "easeInCirc",
+        "easeOutCirc",
+        "easeInOutCirc",
+        "easeInBack",
+        "easeOutBack",
+        "easeInOutBack",
+    ]
+    
+    private init() {}
+    
+    }
+    
+    
+    
+
